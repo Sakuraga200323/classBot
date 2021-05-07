6d18be779dd282e4f8d615969b2dbb15a4441910
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2021-05-07 14:58:48 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,20 +2,15 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+client = discord.Client
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+@client.event
+async def on_message(msg):
+    if msg.content.startswith("t."):
+        if msg.content == "t.ping":
+        await msg.channel.send("Pong!!");
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-bot.run(token)
+client.run(token)
