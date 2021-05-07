572bd16b4520c34fb6709e3731ed671c71eded26
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2021-05-07 15:04:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 
-client = discord.Client
+client = discord.Client(intents=discord.Intents.all())
 token = os.environ['DISCORD_BOT_TOKEN']
 
 @clinent.event
